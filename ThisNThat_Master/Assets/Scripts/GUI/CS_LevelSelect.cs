16bca643375999d5f6e17d95f9022bdc16ee59f3
Josh Brannon <joshmarkbrannon@gmail.com>
2014-08-13 12:09:07 -0400

    Minor Tweaks to Levels Menu

--- ThisNThat_Master/Assets/Scripts/GUI/CS_LevelSelect.cs
+++ ThisNThat_Master/Assets/Scripts/GUI/CS_LevelSelect.cs
@@ -3,21 +3,24 @@ using System.Collections;
 
 public class CS_LevelSelect : MonoBehaviour 
 {
-	
-	public enum LevelType {Cave, Arctic, Forest, Volcano}
+	//Button Variables
+	public enum LevelType {Null, Cave, Arctic, Forest, Volcano}
 	public LevelType Level;
 
+	//World Menu Variables
 	public GameObject World;
 	public float rotationSpeed = 0.5f;
 	public static Quaternion worldRotation;
 		
 	void Start ()
 	{
+		//Initialize the static variable on start
 		CS_LevelSelect.worldRotation = Quaternion.identity;
 	}
 
 	void Update ()
 	{
+		//Perform the rotation while the plane's rotation is not equal to the desired rotation
 		if(World.transform.eulerAngles != CS_LevelSelect.worldRotation.eulerAngles)
 		{
 			World.transform.eulerAngles = Vector3.Slerp(World.transform.eulerAngles, CS_LevelSelect.worldRotation.eulerAngles, rotationSpeed * Time.deltaTime);
@@ -26,21 +29,21 @@ public class CS_LevelSelect : MonoBehaviour
 
 	void OnMouseUp ()
 	{
+		//Determines which button you clicked and adjusts the desired rotation accordingly
 		switch (Level)
 		{
 		case LevelType.Cave:
 			CS_LevelSelect.worldRotation = Quaternion.identity;
-		break;
+			break;
 		case LevelType.Arctic:
-			//World.transform.Rotate(new Vector3(0,0,90));
-			CS_LevelSelect.worldRotation.eulerAngles = new Vector3(0f,0f,90f);
-		break;
+			CS_LevelSelect.worldRotation.eulerAngles = new Vector3(0,0,90);
+			break;
 		case LevelType.Forest:
 			CS_LevelSelect.worldRotation.eulerAngles = new Vector3(0,0,180);
-		break;
+			break;
 		case LevelType.Volcano:
 			CS_LevelSelect.worldRotation.eulerAngles = new Vector3(0,0,270);
-		break;
+			break;
 		}
 	}
 }
