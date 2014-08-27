abdfa8e07261f7f1a63e996bddc3e2c4a58f1404
Josh Brannon <joshmarkbrannon@gmail.com>
2014-08-26 22:12:53 -0400

    Main Menu + Fades

---
+++ ThisNThat_Master/Assets/Scripts/GUI/CS_FadeInOut.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_FadeInOut : MonoBehaviour 
+{
+	public float 		fadeSpeed = 1.5f;
+	private bool 		sceneStarting = true;
+	private bool		sceneEnding = false;
+
+	private CS_MainMenu mainMenu;
+
+	void Awake ()
+	{
+		guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+		mainMenu = GameObject.Find ("Button_Continue").GetComponent<CS_MainMenu>();
+	}
+	
+	void Update()
+	{
+		if(sceneStarting)
+		{
+			StartScene();
+		}
+
+		if(sceneEnding)
+		{
+			EndScene();
+		}
+	}
+
+	void FadeToClear()
+	{
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+	
+	void FadeToBlack()
+	{
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+	
+	void StartScene()
+	{
+		FadeToClear();
+		
+		if(guiTexture.color.a <= 0.05f)
+		{
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+			sceneStarting = false;
+		}
+	}
+	
+	public void EndScene()
+	{
+		guiTexture.enabled = true;
+		sceneEnding = true;
+		FadeToBlack();
+		
+		if(guiTexture.color.a >= 0.95f)
+		{
+			Application.LoadLevel("" + mainMenu.buttonSelected);
+			sceneEnding = false;
+		}
+	}
+}
---
+++ ThisNThat_Master/Assets/Scripts/GUI/CS_MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_MainMenu : MonoBehaviour 
+{
+						public enum 		ButtonType {Null, Continue, NewGame, Options}
+						public ButtonType 	button;
+	[HideInInspector]	public string 		buttonSelected;
+
+						private CS_FadeInOut fadeInOut;
+
+	void Awake()
+	{
+		fadeInOut = GameObject.Find ("PFB_FadeScreen").GetComponent<CS_FadeInOut>();
+	}
+		
+	void OnMouseUp ()
+	{
+		//Determines which button you clicked
+		switch (button)
+		{
+		case ButtonType.Continue:
+			fadeInOut.EndScene();
+			buttonSelected = "UI_LevelSelect";
+			break;
+		case ButtonType.NewGame:
+			//Confirmation Prompt
+			//buttonSelected = "UI_LevelMenu";
+			break;
+		case ButtonType.Options:
+			//Options Menu Appears
+			Application.LoadLevel("UI_LevelSelect");
+			break;
+		}
+	}
+
+
+}
