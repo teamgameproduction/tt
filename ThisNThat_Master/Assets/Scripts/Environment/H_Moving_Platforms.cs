759d7aac793c5180d8b4432ea64f4c19b74d2c82
Nathon Kepley <Spinter92@gmail.com>
2014-09-02 10:36:07 -0400

    Forest Level 1
    A fully traversable level using both players.

---
+++ ThisNThat_Master/Assets/Scripts/Environment/H_Moving_Platforms.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class H_Moving_Platforms : MonoBehaviour 
+{
+
+	float platformSpeed = 0.05f;
+	//public bool isTouched = true;
+	
+	public Transform hPlatform;
+	public Transform hStart;
+	public Transform hEnd;
+
+	Vector3 direction;
+	Transform destination;
+
+	void Start()
+	{
+		SetDestination(hStart);
+	}
+	
+	void Update()
+	{
+		//if (other.gameObject.tag == "Digable")
+		{
+			//isTouched = false;
+				
+			hPlatform.rigidbody.MovePosition (hPlatform.position + direction * platformSpeed);
+				
+			if (Vector3.Distance (hPlatform.position, destination.position) < platformSpeed)
+			{
+					SetDestination(destination == hStart ? hEnd : hStart); 
+			}
+		}
+			
+		//if (other.gameObject.tag == "Digable")
+		//{
+			//isTouched = true;
+			//SetDestination(destination == hStart ? hEnd : hStart); 
+				
+		//}
+	}
+	
+
+	void SetDestination(Transform dest)
+	{
+		destination = dest;
+		direction = (destination.position - hPlatform.position).normalized;
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+	}
+}
+
+	
+
+
