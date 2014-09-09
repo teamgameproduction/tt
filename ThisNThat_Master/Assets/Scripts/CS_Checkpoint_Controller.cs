b350ebe4578b7526984a130caa5f49bcd0799c3e
zoocalu <parkwilkinson91@gmail.com>
2014-09-08 22:28:39 -0400

    park

--- ThisNThat_Master/Assets/Scripts/CS_Checkpoint_Controller.cs
+++ ThisNThat_Master/Assets/Scripts/CS_Checkpoint_Controller.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CS_Checkpoint_Controller : MonoBehaviour 
 {
 	public GameObject[] checkPointArray;
+	public GameObject[] checkPointArray2;
 	public static int currentCheckPoint		=0;
 	public static int currentSpot			=0;
 	public static Vector3 startPosition;
@@ -13,6 +14,17 @@ public class CS_Checkpoint_Controller : MonoBehaviour
 	public GameObject 	gmcharacterBlue;
 	public GameObject 	gmcharacterRed;
 	public int arrayLength = 0;
+	public string s0;
+	public string s1;
+	public string cp1;
+	public string cp2;
+	public string cp3;
+	public string cp4;
+	public string cp5;
+	public string cp6;
+	int r;
+
+
 	int x = 0;
 	
 	// Use this for initialization
@@ -20,10 +32,40 @@ public class CS_Checkpoint_Controller : MonoBehaviour
 		
 	{
 		checkPointArray = GameObject.FindGameObjectsWithTag ("CheckPoint");
+		checkPointArray2 = GameObject.FindGameObjectsWithTag ("CheckPoint");
 		startPosition = transform.position;
 		gmcharacterBlue = GameObject.Find ("characterBlue");
 		gmcharacterRed = GameObject.Find ("characterRed");
 		arrayLength = checkPointArray.Length;
+	
+
+		for(int i = 0; i < checkPointArray.Length; i++)
+		{
+			for(int j = 0; j < checkPointArray.Length; j++)
+			{
+				r=i+1;
+			s0 = r + "";
+			//print (s0);
+			s1 = checkPointArray[j] + "";
+			print (s1);
+			//print (s1.EndsWith(s0));
+				print(s0 + s1[11]);
+				if(s0 == s1[11] + "")
+			{
+				checkPointArray2[i] = checkPointArray[j];
+				print (checkPointArray2[i]);
+			}
+			}
+
+
+		}
+
+		cp1 = checkPointArray2 [0] + "";
+		cp2 = checkPointArray2 [1]+ "";
+		cp3 = checkPointArray2 [2]+ "";
+		cp4 = checkPointArray2 [3]+ "";
+		cp5 = checkPointArray2 [4]+ "";
+		cp6 = checkPointArray2 [5]+ "";
 	}
 	
 	// Update is called once per frame
