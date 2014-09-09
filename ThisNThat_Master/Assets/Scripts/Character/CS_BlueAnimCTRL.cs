57feaebb6252e346f4400b5b98ddddb30d730e17
BGuritza <sinshai936@gmail.com>
2014-09-08 21:12:21 -0400

    Added Blue's mesh and Animtree to Testlevel09

---
+++ ThisNThat_Master/Assets/Scripts/Character/CS_BlueAnimCTRL.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_BlueAnimCTRL : MonoBehaviour {
+	
+	public Animator animator;
+	public CS_FlapReset flapReset;
+	// Use this for initialization
+	void Start () {
+		
+		animator = GetComponent<Animator>();
+		flapReset = GameObject.Find ("characterBlue").GetComponent< CS_FlapReset>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey ("d") && flapReset.Grounded == true || Input.GetKey ("a") && flapReset.Grounded == true){
+			animator.SetBool ("Running", true);
+		}
+		
+		else {
+			animator.SetBool ("Running", false);
+		}
+		
+		if (Input.GetKeyDown ("space") && flapReset.Grounded == true){
+			animator.SetBool ("Jump", true);
+		}
+		
+//		else if (flapReset.Grounded == true){
+//			animator.SetBool ("Jump", false);
+//			animator.SetBool ("Falling", false);
+//		}	
+
+	}
+}
