c7890446c1066e0491c0cce7c3210258e7c74a25
Nathon Kepley <Spinter92@gmail.com>
2014-10-20 18:28:11 -0400

    Forest 3.1
    New Script Added

--- ThisNThat_Master/Assets/Scripts/CS_WinMove.cs
+++ ThisNThat_Master/Assets/Scripts/CS_WinMove.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CS_WinMove : MonoBehaviour {
+public class CS_winMove : MonoBehaviour 
+{
+	public GameObject Winner;
+	public bool win01IsTouch = false;
+	public bool win02IsTouch = false;
+	public bool win03IsTouch = false;
+	public bool win04IsTouch = false;
+	public GameObject winA01;
+	public GameObject winA02;
+	public GameObject winA03;
+	public GameObject winA04;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		Winner = GameObject.Find ("Winner");
+		winA01 = GameObject.Find ("WinAnimation01");
+		winA02 = GameObject.Find ("WinAnimation02");
+		winA03 = GameObject.Find ("WinAnimation03");
+		winA04 = GameObject.Find ("WinAnimation04");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player" && (win01IsTouch = true)) 
+			{
+				Winner.animation.Play("winAnim01");
+			}
+		if (other.gameObject.tag == "Player" && (win02IsTouch = true)) 
+			{
+				Winner.animation.Play("winAnim02");
+			}
+		if (other.gameObject.tag == "Player" && (win03IsTouch = true)) 
+			{
+				Winner.animation.Play("winAnim03");
+			}
+		if (other.gameObject.tag == "Player" && (win04IsTouch = true)) 
+			{
+				Winner.animation.Play("winAnim04");
+			}
+
+
 	}
 }
