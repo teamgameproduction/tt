e45e803a964ab6b581ab745f38c6dd1a9e3b2347
mjohnson093 <michellejohnson093@gmail.com>
2014-10-23 23:15:30 -0400

    Caves LVL01 Version 2 Work in Progress Part2....
    Added in 2nd major part in level. Will continue work in the AM.

---
+++ ThisNThat_Master/Assets/Scenes/UndergroundScene/UndergroundAssets/CS_DestroyWithTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_DestroyWithTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
