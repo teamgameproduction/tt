e4c44c5f1566c6e5fbaf3b9a699b2e64050d338a
Nathon Kepley <Spinter92@gmail.com>
2014-10-27 14:45:27 -0400

    Forest 4.6
    Working on triggered timer script.

---
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_Flytrap.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_Flytrap : MonoBehaviour 
+{
+	public float timer;
+	public float timerLimit = 2.5f;
+	
+	public bool touch;
+	public CS_Checkpoint_Controller cS_CheckPoint_Controller;
+	
+	void Start ()
+	{
+		cS_CheckPoint_Controller = GameObject.Find ("GameState").GetComponent<CS_Checkpoint_Controller>();
+		touch = false;
+	}
+	
+	void Update () 
+	{
+		timer = timer + Time.deltaTime;
+
+		if (timer >= 0.5f) 
+		{
+			timer = timerLimit;
+		}
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		touch = true;
+
+	if (timer <= 0.5f)
+		{
+			cS_CheckPoint_Controller.Die ();
+		}
+
+	if(!other.CompareTag("Player"))
+		{
+			return;
+		}
+		
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			{
+				touch = false;
+			}
+		
+	}
+	
+}
