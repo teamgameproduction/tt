01ac0ecdca3b54e41a8b8d4ec3fbe208feed3372
mjohnson093 <michellejohnson093@gmail.com>
2014-11-07 17:57:12 -0500

    Caves Lighting +Checkpoints+ Fixes

---
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_BlinkingLights.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_BlinkingLights : MonoBehaviour {
+	
+	public float lerpTime = 0.5f;
+	private float i = 0;
+	
+	public int maxDist = 5;
+	public float speed = 40.0f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		light.range = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			light.range = Mathf.PingPong(Time.time * speed, maxDist);
+			
+			i+= Time.deltaTime;
+	}
+	
+}
