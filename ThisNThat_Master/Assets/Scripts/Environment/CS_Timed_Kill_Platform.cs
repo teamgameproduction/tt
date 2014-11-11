5642136626200f3b0abcc7ebc178f14ee5d949e2
Nathon Kepley <Spinter92@gmail.com>
2014-11-10 20:14:21 -0500

    Forest_01
    Name Change.

--- ThisNThat_Master/Assets/Scripts/Environment/CS_Timed_Kill_Platform.cs
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_Timed_Kill_Platform.cs
@@ -5,6 +5,8 @@ public class CS_Timed_Kill_Platform : MonoBehaviour
 {
 	public float timer;
 	public float timerLimit = 5.5f;
+	//public ParticleSystem posionParticle;
+	public GameObject posion;
 
 	public bool touch;
 	public CS_Checkpoint_Controller cS_CheckPoint_Controller;
@@ -12,7 +14,13 @@ public class CS_Timed_Kill_Platform : MonoBehaviour
 	void Start ()
 	{
 		cS_CheckPoint_Controller = GameObject.Find ("GameState").GetComponent<CS_Checkpoint_Controller>();
+		//posionParticle = (ParticleSystem)gameObject.GetComponent("ParticleSystem");
+		posion = GameObject.Find("Death");
+		print ("fjdngjfdnglkjngfdkjnfdngkjfd");
 		touch = false;
+
+		posion.SetActive(false);
+
 	}
 	
 	void Update () 
@@ -26,7 +34,6 @@ public class CS_Timed_Kill_Platform : MonoBehaviour
 			{
 				cS_CheckPoint_Controller.Die ();
 			}
-
 		{
 			if(timer <= 0.5f)
 			{
@@ -36,19 +43,17 @@ public class CS_Timed_Kill_Platform : MonoBehaviour
 		if (timer >= 3.0f) 
 		{
 			renderer.material.color = new Color(0,1,0);
+			posion.SetActive(false);
 		}
-		else if (timer < 3.0f && timer > 1.0f)
-		{
-			renderer.material.color = new Color(1,1,0);
-		}
-		else if (timer <= 1.0f)
+		else if (timer < 2.4f)
 		{
 			renderer.material.color = new Color(1,0,0);
+			posion.SetActive(true);
 		}
 	}
 			
 
-	
+	/*
 	void OnTriggerEnter (Collider other)
 	{
 		touch = true;
@@ -65,7 +70,7 @@ public class CS_Timed_Kill_Platform : MonoBehaviour
 		{
 			touch = false;
 		}
-
 	}
+	*/
 
 }
