be870560a08c57ed7994e1cf58f2ee0b885cfb2a
BGuritza <sinshai936@gmail.com>
2014-11-12 09:04:10 -0500

    Added CS_EnviroRespawn script
    Added script to arctic level

---
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_EnviroRespawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_EnviroRespawn : MonoBehaviour {
+
+	[HideInInspector]	public Vector3 StartPos;
+	public float RespawnTime;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		StartPos = gameObject.transform.position;
+		StartCoroutine ("RespawnTimer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator RespawnTimer(){
+
+		yield return new WaitForSeconds (RespawnTime);
+		gameObject.transform.position = StartPos;
+		StartCoroutine ("RespawnTimer");
+	}
+
+}
