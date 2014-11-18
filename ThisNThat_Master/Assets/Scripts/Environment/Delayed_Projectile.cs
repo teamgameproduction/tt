27e3acf605b924c09857359366a25feb291b867c
Nathon Kepley <Spinter92@gmail.com>
2014-11-17 20:43:34 -0500

    Forest 6.0.2
    More Update

---
+++ ThisNThat_Master/Assets/Scripts/Environment/Delayed_Projectile.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Delayed_Projectile : MonoBehaviour 
+{
+	
+	[HideInInspector]	public Vector3 ProjStartPos;
+	
+	public Vector3 MoveSpeed;
+	public float waitTime;
+
+	public bool shouldMove;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+		//ProjStartPos = gameObject.GetComponent<Transform>;
+		ProjStartPos = gameObject.transform.position;
+		StartCoroutine("MoveTime");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (shouldMove == true)
+		    {
+				MoveXYZ();
+			}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player" || other.gameObject.tag == "Ground") 
+		{
+			gameObject.transform.position = ProjStartPos;
+			shouldMove = false;
+		}
+	}
+	
+	void MoveXYZ()
+	{
+		this.transform.Translate (MoveSpeed.x * Time.deltaTime,MoveSpeed.y * Time.deltaTime, MoveSpeed.z * Time.deltaTime);
+	}
+
+	IEnumerator MoveTime()
+	{
+		yield return new WaitForSeconds (waitTime);
+		shouldMove = true;
+		StopCoroutine("MoveTime");
+		StartCoroutine("MoveTime");
+	}
+}
