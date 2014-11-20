130b58021760f56e374f6ab0cbed14567b3ed553
BGuritza <sinshai936@gmail.com>
2014-11-20 08:52:03 -0500

    Added a WinVolume to take you back to the menu

---
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_Win.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_Win : MonoBehaviour {
+
+	[HideInInspector]	public GameObject gmcharacterRed;
+	[HideInInspector]	public GameObject gmcharacterBlue;
+
+	public bool BlueIsTouching;
+	public bool RedIsTouching;
+
+	// Use this for initialization
+	void Start () {
+	
+		gmcharacterRed = GameObject.Find ("characterRed");
+		gmcharacterBlue = GameObject.Find ("characterBlue");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (RedIsTouching == true && BlueIsTouching == true) {
+
+			Application.LoadLevel("UI_LevelSelect");
+				}
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == gmcharacterRed) {
+
+			RedIsTouching = true;
+
+				} else if (other.gameObject == gmcharacterBlue) {
+			BlueIsTouching = true;
+				}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+
+		if (other.gameObject == gmcharacterRed) {
+			
+			RedIsTouching = false;
+			
+		} else if (other.gameObject == gmcharacterBlue) {
+			BlueIsTouching = false;
+		}
+	}
+}
