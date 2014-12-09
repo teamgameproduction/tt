90dbb94e9e37bd53c61f7abdee743ab777ee11bf
Nathon Kepley <Spinter92@gmail.com>
2014-12-08 21:31:11 -0500

    Forest 6.9
    vfdg

---
+++ ThisNThat_Master/Assets/Scripts/CS_SetActive.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_SetActive : MonoBehaviour 
+{
+	public GameObject objectToSet;
+
+	public GameObject winner;
+
+	public CS_Generic_Pickup pickUp;
+
+	// Use this for initialization
+	void Start () 
+	{
+		objectToSet.SetActive (false);
+		pickUp = GameObject.Find("Winner").GetComponent <CS_Generic_Pickup> ();
+	}
+
+	void Awake ()
+	{
+		objectToSet = GameObject.Find ("PFB_WinVolume");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (pickUp.pickedUp == true)
+		{
+			print ("dfdsfs");
+			objectToSet.SetActive (true);
+		}
+	}
+}
