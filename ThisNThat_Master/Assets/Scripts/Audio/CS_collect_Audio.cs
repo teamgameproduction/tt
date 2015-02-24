cef6c3d798eeee3531bb516e72a784e8fb7dddbf
jgorbach <gorbachjoseph@gmail.com>
2015-02-23 19:45:21 -0500

    Audio Critter Collect & soundscript folder
    Audio script added for the critter collecting. Folder also added just
    for sound audio scripts. Placed script on the critter collect prefab.

---
+++ ThisNThat_Master/Assets/Scripts/Audio/CS_collect_Audio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_collect_Audio : MonoBehaviour {
+	public 	AudioClip[] 		collectSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		int cc = Random.Range (0, collectSound.Length);
+		AudioSource.PlayClipAtPoint(collectSound[cc], transform.position);
+	}
+
+}
