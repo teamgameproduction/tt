0ea4c4daaea4c2f6d56bcf4dfea70dbec590cd4b
jgorbach <gorbachjoseph@gmail.com>
2015-02-24 21:43:16 -0500

    Stalagtite fall script
    Added script for when stalagtite falls to add wistlesound.

---
+++ ThisNThat_Master/Assets/Scripts/Audio/CS_stalagfallSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_stalagfallSound : MonoBehaviour
+{
+
+	public AudioClip[]	stalagDropSnd;
+	/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}*/
+
+	void OnTriggerEnter()
+	{
+		int sf = Random.Range (0, stalagDropSnd.Length);
+		AudioSource.PlayClipAtPoint(stalagDropSnd[sf], transform.position);
+	}
+}
