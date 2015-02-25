58f83d7ccc11c22216358b0f2a4b88f91cb029d8
jgorbach <gorbachjoseph@gmail.com>
2015-02-24 22:03:30 -0500

    watersplash soundscrpt
    created watersplash sound script for arctic level

---
+++ ThisNThat_Master/Assets/Scripts/Audio/CS_WaterSpash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_WaterSpash : MonoBehaviour 
+{
+	public AudioClip[] spashSND;
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		int ss = Random.Range (0, spashSND.Length);
+		AudioSource.PlayClipAtPoint(spashSND[ss], transform.position);
+	}
+}
