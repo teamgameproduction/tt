4723e5bc4114d33e00daa995d16f1af41f5d4068
Josh Brannon <joshmarkbrannon@gmail.com>
2015-03-07 12:30:41 -0500

    Solar System Revamp

---
+++ ThisNThat_Master/Assets/Scenes/GUI/Menu Assets/Waypoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour 
+{
+
+	public Transform referenceLocation;
+	
+	void Update () 
+	{
+		transform.LookAt (referenceLocation);
+	}
+}
---
+++ ThisNThat_Master/Assets/Scripts/GUI/CS_Planet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_Planet : MonoBehaviour 
+{
+
+/*	public enum PlanetType {Null, Forest, Arctic, Caves, Volcano}
+	public PlanetType 	planet;
+	private CS_LevelSelect levelSelect;
+
+	void Awake()
+	{
+		//levelSelect = gameObject.
+	}
+
+	void OnMouseUp ()
+	{
+		switch (planet)
+		{
+		case PlanetType.Forest:
+			Level = 0;
+			break;
+		case PlanetType.Arctic:
+			Level = 1;
+			break;
+		case PlanetType.Caves:
+			Level = 2;
+			break;
+		case PlanetType.Volcano:
+			Level = 3;
+			break;
+		}
+	}*/
+}
