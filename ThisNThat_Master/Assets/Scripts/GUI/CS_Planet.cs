8a2c40aa8dda8057be176823418cd028eb844de3
Josh Brannon <joshmarkbrannon@gmail.com>
2015-03-08 17:50:44 -0400

    Solar System 2.2

--- ThisNThat_Master/Assets/Scripts/GUI/CS_Planet.cs
+++ ThisNThat_Master/Assets/Scripts/GUI/CS_Planet.cs
@@ -7,16 +7,25 @@ public class CS_Planet : MonoBehaviour
 	public enum PlanetType {Null, Forest, Arctic, Cave, Volcano}
 	public PlanetType planet;
 	private CS_LevelSelect levelSelect;
+	public GameObject solarSystem;
 	public Transform referenceLocation;
+
+	private float lerpSpeed = 2.0f;
+	[HideInInspector] public Quaternion endRotation;
 
 	void Awake ()
 	{
 		levelSelect = GameObject.Find("GameState").GetComponent<CS_LevelSelect>();
+		solarSystem = GameObject.Find("Planets");
 	}
 
 	void Update () 
 	{
 		transform.LookAt (referenceLocation);
+		print(solarSystem.transform.eulerAngles);
+		solarSystem.transform.rotation = Quaternion.Slerp(solarSystem.transform.rotation, endRotation, lerpSpeed * Time.deltaTime);
+		//if (endRotation.eulerAngles == solarSystem.transform.eulerAngles)
+		//{	lerpSpeed = 0;	}
 	}
 
 	void OnMouseUp ()
@@ -25,18 +34,30 @@ public class CS_Planet : MonoBehaviour
 		{
 			case PlanetType.Forest:
 				levelSelect.ForestPlanet();
+				endRotation.eulerAngles = new  Vector3 (0,0,0);
 				break;
 			case PlanetType.Arctic:
 				levelSelect.ArcticPlanet();
+				//lerpSpeed = 2.0f;
+				endRotation.eulerAngles = new Vector3 (0,90,0);
 				break;
 			case PlanetType.Cave:
 				levelSelect.CavePlanet();
+				//lerpSpeed = 0;
+				endRotation.eulerAngles = new Vector3 (0,180,0);
+				//lerpSpeed = 2.0f;
 				break;
 			case PlanetType.Volcano:
 				levelSelect.VolcanoPlanet();
+				endRotation.eulerAngles = new Vector3 (0,270,0);
 				break;
 			default:
 				break;
 		}
+	}
+
+	void CalculateRotation()
+	{
+
 	}
 }
