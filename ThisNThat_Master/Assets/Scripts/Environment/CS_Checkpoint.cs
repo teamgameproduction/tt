1bf48d22c317d7ea775da106a22d41870b4fdbed
xXH4tchXx <REgame@stu.aii.edu>
2015-05-11 22:34:18 -0400

    General Update
    New checkpoint (Prefab Complete), Cave integration of the new
    checkpoint, collectibles have been added to the hierarchy but are not
    animated or completed yet.)

--- ThisNThat_Master/Assets/Scripts/Environment/CS_Checkpoint.cs
+++ ThisNThat_Master/Assets/Scripts/Environment/CS_Checkpoint.cs
@@ -11,6 +11,8 @@ public class CS_Checkpoint : MonoBehaviour {
 
 	public bool SetCheckpointForRed = true;
 	public bool SetCheckpointForBlue = true;
+	private Animator CheckPointAnim;
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,8 @@ public class CS_Checkpoint : MonoBehaviour {
 		checkpointCTRL = GameObject.Find ("GameState").GetComponent<CS_CheckpointCTRL> ();
 		gmcharacterBlue = GameObject.Find ("characterBlue");
 		gmcharacterRed = GameObject.Find ("characterRed");
+		CheckPointAnim = gameObject.GetComponent <Animator> ();
+
 	}
 	
 	// Update is called once per frame
@@ -27,23 +31,26 @@ public class CS_Checkpoint : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
 
+		CheckPointAnim.SetBool("Spawn", true);
+
 		if (other.gameObject.tag == "Player") {
 
 			TriggeringPlayer = other.gameObject;
 
 			if(TriggeringPlayer == gmcharacterRed && SetCheckpointForRed == true){
-				checkpointCTRL.CurrentCheckpointRed = this.transform.position;
+				checkpointCTRL.CurrentCheckpointRed = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z -1.5f);
 
 				if(SetCheckpointForBlue == true){
-					checkpointCTRL.CurrentCheckpointBlue = this.transform.position;
+
+					checkpointCTRL.CurrentCheckpointBlue = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z -1.5f);
 				}
 			}
 
 			else if(TriggeringPlayer == gmcharacterBlue && SetCheckpointForBlue == true){
-				checkpointCTRL.CurrentCheckpointBlue = this.transform.position;
+				checkpointCTRL.CurrentCheckpointBlue = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z -1.5f);
 
 				if(SetCheckpointForRed == true){
-					checkpointCTRL.CurrentCheckpointRed = this.transform.position;
+					checkpointCTRL.CurrentCheckpointRed = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z -1.5f);
 				}
 			}
 
---
+++ ThisNThat_Master/Assets/SharedAssets/Checkpoint/New_Check_Anim.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class New_Check_Anim : MonoBehaviour {
+
+	private Animator CheckPointAnim;
+
+	void Start () {
+
+		CheckPointAnim = gameObject.GetComponent <Animator> ();
+	}
+
+	void OnCollisionEnter (){
+
+		CheckPointAnim.SetBool("Spawn", true);
+
+
+		}
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp ("u")) {
+
+			CheckPointAnim.SetBool("Spawn", true);
+
+		}
+	}
+}
